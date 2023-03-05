9b1a095e002f886e62a3d7fc5d74afdd953cdfa2
Sebastian Hirt <sebastian@hirts.de>
2023-03-05 02:12:42 +0100

    changed/fixed gallery, fixed some stuff

--- js/master_1.1.js
+++ js/master_1.1.js
@@ -1,16 +1,7 @@
-
 var pos = "0";
 
 function navbar(){
-    /*var x=document.documentElement.scrollTop || document.body.scrollTop;
-    if (x<pos){
-        document.getElementById("myTopnav").style.animationPlayState="running";
-    }
-    pos=x;*/
-
     document.getElementById("myTopnav").style.animationPlayState="running";
-
-
 }
 
 function impr() {
@@ -29,30 +20,28 @@ function ini() {
     document.getElementById("blur").style.display = "none";
     document.getElementById("blur2").style.display = "none";
     document.getElementById("age").innerHTML = getAge(new Date(1967,12,16));
-    resize();
 }
 
 
 function bigimg(a) {
     if (document.getElementById("blur2").style.display == "none") {
 
-        if (a=="img2"|| a=="img3"){
+        var w = document.getElementById(a).width;
+        var h = document.getElementById(a).height;
+
+        if (w>h) {
+            document.getElementById("bigimg1").style.width="70%";
+            document.getElementById("bigimg1").style.height="auto";
+        } else {
             document.getElementById("bigimg1").style.height="70%";
             document.getElementById("bigimg1").style.width="auto";
         }
-        else{
-            document.getElementById("bigimg1").style.height="auto";
-            document.getElementById("bigimg1").style.width="70%";
-        }
-
-        document.getElementById("blur2").style.display = "block";
         document.getElementById("bigimg1").src=document.getElementById(a).dataset.src;
+        document.getElementById("blur2").style.display = "block";
     }
-    else{
+    else {
         document.getElementById("blur2").style.display = "none";
-
     }
-
 }
 
 
@@ -65,35 +54,13 @@ function navsmall(y) {
     } else {
         x.className = "topnav";
     }
-
-
 }
 
 function navb() {
     document.getElementById("myTopnav").className="topnav";
     document.getElementById("navbut").className="container";
-
-
 }
 
-
-function resize() {
-
-    var w=window.innerWidth;
-    var hges=0;
-    if (w>=1300){
-        w=w/2;
-        var hges= ((((w*0.4)*1.5)+((w*0.4)/1.5))/0.8)+"px";
-    }
-    else {
-        var hges= ((((w*0.4)*1.5)+((w*0.4)/1.5))/0.8)+"px";
-    }
-
-
-
-    document.getElementById("cont2").style.height=(hges);
-
-}
 
 function getAge(dob) { 
       var diff_ms = Date.now() - dob.getTime();
